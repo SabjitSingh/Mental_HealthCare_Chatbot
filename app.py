73f287eb329432f6f6aba0574fab4e0c247fd229
Sabjit singh <saabdhillon2002@gmail.com>
2024-05-06 12:53:30 +0530

    UI Update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 
-# Load your pre-trained model
+# # Load your pre-trained model
 pipeline = joblib.load("svm_model.joblib")
 
 # Function to predict response
@@ -9,16 +9,32 @@ def predict_input_svm(pipeline, user_input):
     predicted_response = pipeline.predict([user_input])[0]
     return predicted_response
 
-# Define Streamlit UI
+# # Define Streamlit UI
 st.title("Mental Healthcare Chatbot")
 
-# Add a text input field for user input
-user_input = st.text_input("You:", "")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# Add a button to submit user input
-if st.button("Send"):
-    # Predict response based on user input
-    bot_response = predict_input_svm(pipeline, user_input)
-    
-    # Display bot response
-    st.text_area("Bot:", value=bot_response, height=100)
+# Display chat messages from history on app rerun
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Enter your input here"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response =predict_input_svm(pipeline, prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
