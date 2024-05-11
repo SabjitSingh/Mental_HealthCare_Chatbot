fd471b23e54a07f5df3bf1df9648809cc990af0c
Sabjit singh <saabdhillon2002@gmail.com>
2024-05-11 22:35:56 +0530

    model change

--- app.py
+++ app.py
@@ -2,38 +2,37 @@ import streamlit as st
 import joblib
 
 # # Load your pre-trained model
-pipeline = joblib.load("DecisionTree.joblib")
+pipeline = joblib.load("svm_model.joblib")
 
-# Function to predict response
+# Function for prediction the answer
 def predict_input_svm(pipeline, user_input):
     predicted_response = pipeline.predict([user_input])[0]
     return predicted_response
 
-# # Define Streamlit UI
+# Chatbot ui 
 st.title("Mental Healthcare Chatbot")
 st.markdown("Hello! I'm your friendly mental health care assistant.")
-# Initialize chat history
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-
+# Display chat messages from history 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# React to user input
+
 if prompt := st.chat_input("Enter your input here"):
-    # Display user message in chat message container
+    
     st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
+   
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     response =predict_input_svm(pipeline, prompt)
-    # Display assistant response in chat message container
+    
     with st.chat_message("assistant"):
         st.markdown(response)
-    # Add assistant response to chat history
+
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
